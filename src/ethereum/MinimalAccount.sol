fa563ce1841a06b080012ebd2c2468762ebd6c32
Malcolm Tan <malcolmtan@Malcolms-MacBook-Air.local>
2024-08-13 01:40:33 +0800

    Update

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -2,5 +2,12 @@
 pragma solidity 0.8.24;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
-contract MinimalAccount is IAccount {}
+contract MinimalAccount is IAccount {
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData) {}
+}
